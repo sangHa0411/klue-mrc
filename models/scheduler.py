9f14e39adb585345b25a825d2ffe8f215aa439da
sangha110495 <48673702+sangHa0411@users.noreply.github.com>
2022-08-12 13:43:28 +0900

    Update scheduler.py

--- models/scheduler.py
+++ models/scheduler.py
@@ -4,11 +4,13 @@ import torch
 class LinearWarmupScheduler(torch.optim.lr_scheduler.LambdaLR):
     def __init__(self, optimizer, total_steps, warmup_ratio, last_epoch=-1):
 
-        def lr_lambda(step):
-            warmup_steps = int(total_steps * warmup_ratio)            
-            if step < warmup_steps:
-                return float(step) / float(warmup_steps)
-            else :
-                return (float(total_steps) - float(step)) / float(total_steps - warmup_steps)
+        def lr_lambda(current_step: int):
+            warmup_steps = int(total_steps * warmup_ratio)
 
-        super(LinearWarmupScheduler, self).__init__(optimizer, lr_lambda, last_epoch=last_epoch)
+            if current_step < warmup_steps:
+                return float(current_step) / float(max(1, warmup_steps))
+            return max(
+                0.0, float(total_steps - current_step) / float(max(1, total_steps - warmup_steps))
+            )
+
+        super(LinearWarmupScheduler, self).__init__(optimizer, lr_lambda, last_epoch=last_epoch) 
